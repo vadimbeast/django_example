ee8e0fe91338ce4dbeb7c27fb192c838c93769ce
drjekill218 <vadimselivyorstov94@gmail.com>
2021-12-29 16:01:45 +0300

    django_example
    django_example

---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.http.response import HttpResponseBadRequest
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+# Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'    
+
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/create.html', context)    
